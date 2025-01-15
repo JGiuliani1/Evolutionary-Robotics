db09bb2c38ac54cb57fedd8350b59a8fcb145670
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-15 17:25:16 -0500

    hide sidebar command commented out

--- simulation.py
+++ simulation.py
@@ -4,12 +4,13 @@ import time
 # create client to connect with pybullet
 physicsClient = p.connect(p.GUI)
 # hide sidebar
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 # step the physics 1000 times
 for i in range(0, 1000):
     p.stepSimulation()
     time.sleep(1/60)
+    print(i)
 
 # disconnect pybullet client
 p.disconnect()
