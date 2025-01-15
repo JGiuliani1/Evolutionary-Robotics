bed6f994eb0eb08f9dfb1c04b2618e5002022907
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-15 17:19:37 -0500

    step simulation in graphics window

--- simulation.py
+++ simulation.py
@@ -1,4 +1,15 @@
 import pybullet as p
+import time
 
+# create client to connect with pybullet
 physicsClient = p.connect(p.GUI)
+# hide sidebar
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# step the physics 1000 times
+for i in range(0, 1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+
+# disconnect pybullet client
 p.disconnect()
