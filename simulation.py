99c9c71a24d89a0e72b69a13587b5110e4df7372
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-22 14:22:56 -0500

    Added gravity and floor

--- simulation.py
+++ simulation.py
@@ -1,11 +1,18 @@
 import pybullet as p
+import pybullet_data
 import time
 
 # create client to connect with pybullet
 physicsClient = p.connect(p.GUI)
+# path for pybullet_data
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # hide sidebar
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+# add gravity
+p.setGravity(0,0,-9.8)
+# set floor
+planeID = p.loadURDF("plane.urdf")
 # read world from box.sdf
 p.loadSDF("box.sdf")
 # step the physics 1000 times
