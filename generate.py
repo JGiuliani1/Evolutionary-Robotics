eaff684ffb82e92db3ee4ce869f114210666a6ba
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-22 17:52:25 -0500

    Block generates above floor

--- generate.py
+++ generate.py
@@ -3,6 +3,12 @@ import pyrosim.pyrosim as pyrosim
 # create file to information about link
 pyrosim.Start_SDF("box.sdf")
 # store box size and position
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 2
+height = 3
+x = 0
+y = 0
+z = 1.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 # end program and close sdf file
 pyrosim.End()
