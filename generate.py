6b4920d33c2c1f413d926a05ceba0924fe4a89f8
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-22 14:09:16 -0500

    small changes

--- generate.py
+++ generate.py
@@ -1,8 +1,8 @@
 import pyrosim.pyrosim as pyrosim
 
-# create file to store information about world
+# create file to information about link
 pyrosim.Start_SDF("box.sdf")
-# store box position
+# store box size and position
 pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
-# end simulation and close sdf file
+# end program and close sdf file
 pyrosim.End()
