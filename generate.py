dd410da448a1cbc8ed33b4953c2f6bb2d815e3fa
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-22 18:23:40 -0500

    Boxes get smaller as tower gets taller

--- generate.py
+++ generate.py
@@ -9,8 +9,12 @@ height = 1
 x = 0
 y = 0
 z = .5
+
 # create 10 boxes stacked ontop of one another
 for i in range(0, 10):
     pyrosim.Send_Cube(name="Box", pos=[x,y,z+i] , size=[length,width,height])
+    length = length * .9
+    width = width * .9
+    height = height * .9
 # end program and close sdf file
 pyrosim.End()
