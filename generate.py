b0b8cbf900f4705038557865f88200976b85aa02
JGiuliani1 <jbgiulia@uvm.edu>
2025-01-29 15:27:27 -0500

    leg connected to torso

--- generate.py
+++ generate.py
@@ -9,16 +9,9 @@ def main():
 def Create_World():
     # create file to information about link
     pyrosim.Start_SDF("world.sdf")
-    # store box size and position
-    length = 1
-    width = 1
-    height = 1
-    x = -2
-    y = 2
-    z = .5
 
-    # generate box at origin
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    # generate box
+    pyrosim.Send_Cube(name="Box", pos=[-2,-2,.5] , size=[1,1,1])
 
     # end program and close sdf file
     pyrosim.End()
@@ -26,16 +19,15 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    # store box size and position
-    length = 1
-    width = 1
-    height = 1
-    x = 0
-    y = 0
-    z = .5
 
-    # generate box at origin
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    # generate torso
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,.5] , size=[1,1,1])
+    # joint to connect leg and torso
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position=[0,0,1])
+    # generate leg
+    pyrosim.Send_Cube(name="Leg", pos=[0,0,.5], size=[1,1,1])
+
+
     pyrosim.End()
 
 
