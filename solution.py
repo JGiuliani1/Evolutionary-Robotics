fb281b8a241a34b042993590e4f6317c690cea29
JGiuliani1 <jbgiulia@uvm.edu>
2025-03-24 11:50:28 -0400

    updated front leg

--- solution.py
+++ solution.py
@@ -48,15 +48,15 @@ class SOLUTION:
         pyrosim.Start_URDF("body.urdf")
 
         # torso
-        pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
+        pyrosim.Send_Cube(name="Torso", pos=[0,0,1], size=[1,1,1])
         # joint to connect torso to back leg
         pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1,0,1])
         # back leg
-        pyrosim.Send_Cube(name="BackLeg", pos=[-.5,0,-.5])
+        pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5])
         # joint to connect torso to front leg
-        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2,0,1])
+        pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0,0.5,1])
         # front leg
-        pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5])
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0], size=[0.2,1,0.2])
 
         pyrosim.End()
 
