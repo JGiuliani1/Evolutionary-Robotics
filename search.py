11644971784ddd01d18e489cf1683f4c4c1c64d5
JGiuliani1 <jbgiulia@uvm.edu>
2025-03-24 11:12:04 -0400

    small changes

--- search.py
+++ search.py
@@ -1,10 +1,6 @@
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import os
 
-#for i in range(0, 5):
-#    os.system("python generate.py")
-#    os.system("python simulate.py")
-
 phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
